9ea492ea6a46aaf903e2d7f82c7abb5fa2365845
Nato <ivannatnat0@gmail.com>
2022-09-27 12:45:17 +0800

    Adutucart User First Stage

---
+++ app/src/main/java/com/example/adutucart5/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.adutucart5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.adutucart5.databinding.ActivityMainBinding
+import com.example.adutucart5.fragment.CartFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment -> "My Cart"
+                    R.id.moreFragment -> "My DashBoard"
+                    else -> "Adutucart"
+                }
+            }
+
+        })
+    }
+}
